e2d612dc8c9a081bc0fcbaacf683bf151f7bcf84
chenyuqide <chenyuqide@outlook.com>
2020-11-24 00:31:08 +0800

    Update: first update config

--- config.h
+++ config.h
@@ -1,12 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Firacode:size=10" };
+static const char dmenufont[]       = "Firacode:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -44,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,60 +58,101 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+static const char *termcmd[]    = { "st", NULL };
+static const char *network[]    = { "st","nmtui", NULL };
+static const char *browser[]    = { "firefox", NULL };
+static const char *browserout[] = { "firefox-developer-edition", NULL };
+static const char *music[]      = { "netease-cloud-music", NULL };
+static const char *file[]       = { "rang", NULL };
+static const char *le[]         = { "rang", NULL };
+static const char *volumn[]     = { "pavucontrol-qt", NULL };
+
+static const char *vmute[]      = { "amixer", "set", "Master", "toggle", NULL};
+static const char *vumnup[]     = { "amixer", "set", "Master", "10%+", NULL};
+static const char *vdown[]      = { "amixer", "set", "Master", "10%-", NULL};
+static const char *bldown[]     = { "xbacklight", "-dec", "10", NULL};
+static const char *blup[]       = { "xbacklight", "-inc", "10", NULL};
+
+static const char *lock[]       = { "blurlock", NULL};
+
+#define M | MODKEY
+#define S | ShiftMask
+#define C | ControlMask
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier   key                      function        argument */
+	{ 0 M,        XK_d,                    spawn,          {.v = dmenucmd } },
+	{ 0 M,        XK_Return,               spawn,          {.v = termcmd } },
+	{ 0 M,        XK_u,                    spawn,          {.v = browser} },
+	{ 0 M S,      XK_u,                    spawn,          {.v = browserout} },
+	{ 0 M,        XK_i,                    spawn,          {.v = music} },
+	{ 0 M,        XK_e,                    spawn,          {.v = file} },
+	{ 0 M,        XK_n,                    spawn,          {.v = network} },
+
+    { 0,          XF86XK_AudioLowerVolume, spawn,          {.v = vdown} },
+    { 0,          XF86XK_AudioRaiseVolume, spawn,          {.v = vumnup} },
+    { 0,          XF86XK_AudioMute,        spawn,          {.v = vmute} },
+    { 0,          XF86XK_MonBrightnessDown,spawn,          {.v = bldown} },
+    { 0,          XF86XK_MonBrightnessUp,  spawn,          {.v = blup} },
+
+    { 0 M,          XK_BackSpace,            spawn,          {.v = blup} },
+
+	{ 0 M,        XK_j,                    focusstack,     {.i = +1 } },
+	{ 0 M,        XK_k,                    focusstack,     {.i = -1 } },
+	{ 0 M S,      XK_j,                    incnmaster,     {.i = +1 } },
+	{ 0 M S,      XK_k,                    incnmaster,     {.i = -1 } },
+
+	{ 0 M,        XK_h,                    setmfact,       {.f = -0.05} },
+	{ 0 M,        XK_l,                    setmfact,       {.f = +0.05} },
+	{ 0 M S,      XK_Return,               zoom,           {0} },
+	{ 0 M,        XK_Tab,                  view,           {0} },
+	{ 0 M S,      XK_q,                    killclient,     {0} },
+	{ 0 M,        XK_z,                    setlayout,      {.v = &layouts[0]} },
+	{ 0 M,        XK_x,                    setlayout,      {.v = &layouts[1]} },
+	{ 0 M,        XK_c,                    setlayout,      {.v = &layouts[2]} },
+	{ 0 M,        XK_space,                setlayout,      {0} },
+	{ 0 M S,      XK_space,                togglefloating, {0} },
+	{ 0 M,        XK_0,                    view,           {.ui = ~0u } },
+	{ 0 M S,      XK_0,                    tag,            {.ui = ~0u } },
+
+	//{ 0 M,        XK_comma,                viewnext,       {.i = -1 } },
+	//{ 0 M,        XK_period,               viewnext,       {.i = +1 } },
+	//{ 0 M S,      XK_comma,                viewnext,       {.i = -3 } },
+	//{ 0 M S,      XK_period,               viewnext,       {.i = +3 } },
+
+
+	{ 0 M S,      XK_e,                    quit,           {0} },
+	{ 0 M S C,    XK_e,                    quit,           {1} }, 
+	//{ 0,          MODKEY,                 holdbar,        {0} },
+
+	TAGKEYS(      XK_1,                    0)
+	TAGKEYS(      XK_2,                    1)
+	TAGKEYS(      XK_3,                    2)
+	TAGKEYS(      XK_4,                    3)
+	TAGKEYS(      XK_5,                    4)
+	TAGKEYS(      XK_6,                    5)
+	TAGKEYS(      XK_7,                    6)
+	TAGKEYS(      XK_8,                    7)
+	TAGKEYS(      XK_9,                    8)
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    /* click                event mask      button          function argument
+     */
+    {ClkLtSymbol,   0   ,   Button1,        setlayout,      {0}},
+    {ClkLtSymbol,   0   ,   Button3,        setlayout,      {.v = &layouts[2]}},
+    {ClkWinTitle,   0   ,   Button2,        zoom,           {0}},
+    {ClkStatusText, 0   ,   Button2,        spawn,          {.v = termcmd}},
+    {ClkClientWin,  0  M,   Button1,        movemouse,      {0}},
+    {ClkClientWin,  0  M,   Button2,        togglefloating, {0}},
+    {ClkClientWin,  0  M,   Button3,        resizemouse,    {0}},
+    {ClkTagBar,     0   ,   Button1,        view,           {0}},
+    {ClkTagBar,     0   ,   Button3,        toggleview,     {0}},
+    {ClkTagBar,     0  M,   Button1,        tag,            {0}},
+    {ClkTagBar,     0  M,   Button3,        toggletag,      {0}},
 };
-
